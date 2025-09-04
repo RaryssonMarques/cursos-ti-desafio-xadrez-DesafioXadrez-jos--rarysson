45a69d723d539a62e70e583ed112ad03b3b0687c
RaryssonMarques <raryssonmarques2200@gmail.com>
2025-09-04 23:46:48 +0000

    criei o printf

--- xadrez.c
+++ xadrez.c
@@ -4,8 +4,10 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-   printf("desafio xadrez!"); // Nível Novato - Movimentação das Peças
+int main() { 
+   printf("desafio xadrez!"); 
+   return 0;
+    // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
