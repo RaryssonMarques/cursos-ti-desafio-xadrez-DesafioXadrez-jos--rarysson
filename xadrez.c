e57feedac1d6a02a7c59ae81c278397ce1308ae1
RaryssonMarques <raryssonmarques2200@gmail.com>
2025-09-06 01:20:28 +0000

    novo commit

--- xadrez.c
+++ xadrez.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include <stdio.h>;
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo. 
 
 int main() { 
    printf("desafio xadrez!"); 
    return 0;
+
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
